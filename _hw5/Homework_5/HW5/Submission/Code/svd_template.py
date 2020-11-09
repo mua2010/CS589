72d8ac90fba9c78464ee3f01fbad5052b71cdf41
Umair Afzal <mumairafzal2010@gmail.com>
2020-11-09 17:09:33 -0500

    n

--- _hw5/Homework_5/HW5/Submission/Code/svd_template.py
+++ _hw5/Homework_5/HW5/Submission/Code/svd_template.py
@@ -1,54 +1,90 @@
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import math
-def SVD(A, s, k):
-    # TODO: Calculate probabilities p_i
-    n,m = A.shape
-    probabilities = list()
-    counter = 0
-    while counter < n:
-        num = math.pow(np.linalg.norm(A[counter,:], ord=2), 2)
-        dnm = np.linalg.norm(A)**2
-        pi = num/dnm
-        probabilities.append(pi)
-        counter += 1
-    # TODO: Construct S matrix of size s by m
-    S = np.zeros((s,m))
+# import numpy as np
+# from PIL import Image
+# import matplotlib.pyplot as plt
+# import math
+# def SVD(A, s, k):
+#     # TODO: Calculate probabilities p_i
+#     n,m = A.shape
+#     probabilities = list()
+#     counter = 0
+#     while counter < n:
+#         upper = math.pow(np.linalg.norm(A[counter,:], ord=2), 2)
+#         lower = math.pow(np.linalg.norm(A), 2)
+#         probabilities.append(upper / lower)
+#         counter += 1
+#     # TODO: Construct S matrix of size s by m
+#     S = np.zeros((s,m))
+#     counter = 0
+#     while counter < s:
+#         random_choice = np.random.choice(list(range(n)), p=probabilities)
+#         S[counter] = A[random_choice,:]
+#         counter += 1
 
-    # TODO: Calculate SS^T
+#     # TODO: Calculate SS^T
+#     ss_to_t = np.matmul(S, S.T)
+
+#     # TODO: Compute SVD for SS^T
+#     svd = np.linalg.svd(ss_to_t)
+#     W = svd[0]
+#     values = svd[1]
+
+#     # TODO: Construct H matrix of size m by k
+#     H = np.zeros((k, m))
+#     counter = 0
+#     while counter < k:
+#         mat_mul = np.matmul(S.T, W[:, counter])
+#         linalg_norm = np.linalg.norm(mat_mul, ord=2)
+#         H[counter] = mat_mul / linalg_norm
+#         counter += 1
+#     values.sort()
+#     values = values[::-1]
+#     top_k_singular_values_sigma = values[:k]
+
+#     # Return matrix H and top-k singular values sigma
+#     return H.T, top_k_singular_values_sigma
+
+# def main():
+#     im = Image.open("../../Data/baboon.tiff")
+#     A = np.array(im)
+#     k = 60
 
 
-    # TODO: Compute SVD for SS^T
+#     # TO DO: Compute SVD for A and calculate optimal k-rank approximation for A.
+#     linalg_svd = np.linalg.svd(A)
+#     U = linalg_svd[0]
+#     S = np.array(linalg_svd[1][:k])
+#     Vh = linalg_svd[2]
+#     lamda_matrix = np.zeros((512,512))
+#     counter = 0
+#     while counter < k:
+#         lamda_matrix[counter][counter] = S[counter]
+#         counter += 1
+#     optimal = np.matmul(np.matmul(U, lamda_matrix[:, :k]), Vh[:k, :])
+#     H, sigma = SVD(A, 80, 60)
 
+#     # TO DO: Use H to compute sub-optimal k rank approximation for A
+#     sub_optimal = np.matmul(np.matmul(A, H), H.T)
 
-    # TODO: Construct H matrix of size m by k
-    H = np.zeros((m,k))
+#     # To DO: Generate plots for original image, optimal k-rank and sub-optimal k rank approximation
+#     plt.title("original Image")
+#     plt.imshow(A)
+#     plt.savefig('../Figures/q2.3a_orignal.png')
 
+#     plt.title("optimal k-rank")
+#     plt.imshow(optimal)
+#     plt.savefig('../Figures/q2.3a_optimal_k_rank.png')
 
-    # Return matrix H and top-k singular values sigma
-    
+#     plt.title("sub-optimal k-rank")
+#     plt.imshow(sub_optimal)
+#     plt.savefig('../Figures/q2.3a_sub_optimal_k_rank.png')
 
-def main():
-    im = Image.open("baboon.tiff")
-    A = np.array(im)
-    H, sigma = SVD(A, 80, 60)
-    k = 60
+#     # TO DO: Calculate the error in terms of the Frobenius norm for both the optimal-k
+#     # rank produced from the SVD and for the k-rank approximation produced using
+#     # sub-optimal k-rank approximation for A using H.
+#     print("Errors Q2.3.b")
+#     print(np.linalg.norm(optimal - A, ord='fro'))
+#     # sub-optimal k-rank approximation for A using H.
+#     print(np.linalg.norm(sub_optimal - A, ord='fro'))
 
-
-    # TO DO: Compute SVD for A and calculate optimal k-rank approximation for A.
-
-
-    # TO DO: Use H to compute sub-optimal k rank approximation for A
-
-
-    # To DO: Generate plots for original image, optimal k-rank and sub-optimal k rank approximation
-
-
-    # TO DO: Calculate the error in terms of the Frobenius norm for both the optimal-k
-    # rank produced from the SVD and for the k-rank approximation produced using
-    # sub-optimal k-rank approximation for A using H.
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
